76334386d1a25c2b9bd55f44db889a32ed6d6c67
faridmustafayev <feridmustafayev34@gmail.com>
2024-07-18 00:40:43 +0400

    Revert "UserNotFoundException added"
    This reverts commit 3cc3be2fff62531790aa877365f552492a9c9be7.

--- src/main/java/com/example/springproject/model/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springproject.model.exception;
-
-public class NotFoundException extends RuntimeException{
-    private String message;
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
